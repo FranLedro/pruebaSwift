4fbfc1c0a506c842bebe35dfd4575560026ce520
Fran Ledro <FranLedro@fran-ledro.local>
2015-12-23 00:06:54 +0100

    Initial Commit

---
+++ Prueba/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  Prueba
+//
+//  Created by Fran Ledro on 23/12/15.
+//  Copyright © 2015 Fran Ledro. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
